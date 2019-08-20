371fb019439f411ccb8bb275fc7b5e932f90e3a3
Felix Wong <waf@fawong.com>
2019-08-20 21:12:02 +0000

    add back thermostat demo

--- demo.py
+++ demo.py
@@ -15,13 +15,20 @@ thermostat = panels[0].get_devices(device_type_set=[
     vivint.VivintCloudSession.VivintDevice.DEVICE_TYPE_THERMOSTAT
 ])[0]
 
+# In this case, get the first door from the first site
+door = panels[0].get_devices(device_type_set=[
+    vivint.VivintCloudSession.VivintDevice.DEVICE_TYPE_DOOR_LOCK
+])[0]
+
 # Get the current state and print it out
 print(thermostat.current_state())
 
+print("Door state:", door.current_state())
+
 # Set a few thermostat things.
-thermostat.set_operation_mode("heat")
-thermostat.set_fan_mode("always")
-thermostat.set_temperature(10)
+# thermostat.set_operation_mode("heat")
+# thermostat.set_fan_mode("always")
+# thermostat.set_temperature(10)
 
 # Let the change propagate for a bit
 time.sleep(2)
